84daaae2406fbbadf0c680a47b3d9bc70b33ac0a
Lizi956 <lizikenchadze2006@gmail.com>
2025-01-22 06:03:22 -0800

    new

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-
 let button = document.getElementById("button");
-
 button.addEventListener("click", function () {
   alert("click");
 });
-
-
-const burgerBar = document.getElementById('burgerBar');
-const menu = document.getElementById('menu');
-
-// Toggle menu visibility on burger click
-burgerBar.addEventListener('click', () => {
-    menu.classList.toggle('active');
+let burgerBar = document.getElementById("burgerBar");
+let menu = document.getElementById("menu");
+burgerBar.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
